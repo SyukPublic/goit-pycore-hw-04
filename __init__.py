121b0b3303096092bf68a4891f1798d215e7d5c8
roman <galas@incust.com>
2025-06-12 22:17:15 +0300

    Task 04 added

--- __init__.py
+++ __init__.py
@@ -6,5 +6,6 @@ __author__ = 'Roman'
 from tasks.task_01 import total_salary
 from tasks.task_02 import get_cats_info
 from tasks.task_03 import print_directory_tree
+from tasks.task_04 import main as contacts_bot
 
-__all__ = ['total_salary', 'get_cats_info', 'print_directory_tree']
+__all__ = ['total_salary', 'get_cats_info', 'print_directory_tree', 'contacts_bot']
